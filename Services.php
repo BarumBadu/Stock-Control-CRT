df6d97e9c8a277dbe4868482cdd2a502b54b2e85
BarumBadu <85981621+BarumBadu@users.noreply.github.com>
2023-04-27 19:56:54 +0800

    Add files via upload

---
+++ Services.php
@@ -0,0 +1,79 @@
+<?php include_once('landingheader.php'); ?>
+
+
+     <!-- ======= About Section ======= -->
+     <section id="about" class="about">
+      <div class="container align-content-center justify-content-center" data-aos="fade-up">
+
+
+
+        <div class="row">
+
+          <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <div class="section-title text-center">
+              <h2>Grade 11 & 12</h2>
+              <p>Senior High School</p>
+
+            </div>
+            <ul class = "">
+              <h2>Academic Track</h2>
+              <li><i class="ri-check-double-line text-primary"></i> Accountancy, Business and Management (ABM)</li>
+              <li><i class="ri-check-double-line text-primary"></i> General Academic Track (GAS)</li>
+              <li><i class="ri-check-double-line text-primary"></i> Humanities and Social Sciences (HUMSS)</li>
+              <li><i class="ri-check-double-line text-primary"></i> Science, Technology, Engineering and Mathematics (STEM)</li>
+            </ul>
+            <a class = "btn btn-primary">Enroll Now !</a>
+          </div>
+          
+          <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <div class="section-title text-center">
+              <h2>4 and 2 Years</h2>
+              <p>College</p>
+            </div>
+            <ul class = "">
+              <h2>4 YEARS COURSE</h2>
+              <li><i class="ri-check-double-line text-primary"></i> B.S. in Information Technology</li>
+              <li><i class="ri-check-double-line text-primary"></i> B.S. in Accounting Information System</li>
+              <li><i class="ri-check-double-line text-primary"></i> B.S. in Hospitality Management</li>
+              <li><i class="ri-check-double-line text-primary"></i> B.S. in Tourism Management</li>
+              <li><i class="ri-check-double-line text-primary"></i> Bachelor in Technical Vocational Teacher Education</li>
+            </ul>
+            <a class = "btn btn-primary">Enroll Now !</a>
+          </div>
+        </div>
+
+
+        <div class="row">
+
+          <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <div class="section-title text-center">
+         
+           
+            </div>
+            <ul class = "">
+              <h2>Technical-Vocational-Livelihood</h2>
+              <li><i class="ri-check-double-line text-primary"></i> Associate in Computer Technology</li>
+              <li><i class="ri-check-double-line text-primary"></i> Hotel and Restaurant Services</li>
+              <li><i class="ri-check-double-line text-primary"></i> Food Service Management</li>
+            </ul>
+            <a class = "btn btn-primary">Enroll Now !</a>
+          </div>
+          <br><bR>
+          <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <div class="section-title text-center">
+           
+            </div>
+            <ul class = "">
+              <h2>2 YEARS COURSE</h2>
+              <li><i class="ri-check-double-line text-primary"></i> Information and Communications Technology (ICT)</li>
+              <li><i class="ri-check-double-line text-primary"></i> Home Economics (HE)</li>
+
+            </ul>
+            <a class = "btn btn-primary">Enroll Now !</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <?php include_once('landingfooter.php'); ?>
